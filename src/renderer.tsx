7329b7c0d09ebaab428b5a69311681c3e79acc80
singhvigyat <code.vigyat@gmail.com>
2025-11-18 01:13:26 +0530

    feat: add student dashboard, add dark mode

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -4,14 +4,16 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 
 // Import your components
-import Dashboard from './components/StudentDashboard.tsx';
+import Dashboard, { StudentDashboard } from './components/StudentDashboard.tsx';
 import { TeacherDashboard } from './components/TeacherDashboard.tsx';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<TeacherDashboard/>}  >
+        <Route path="/" element={<StudentDashboard/>}  >
+        {/* <Route path="/" element={<TeacherDashboard/>}  > */}
+
           {/* <Route path="settings" element={<Settings />} /> */}
         </Route>
       </Routes>
